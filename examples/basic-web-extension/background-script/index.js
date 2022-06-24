8e0c5ae21b3c7d326df530744773ef0294dff9c5
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-06-24 16:35:12 +0200

    Extend basic example with foreground script

--- examples/basic-web-extension/background-script/index.js
+++ examples/basic-web-extension/background-script/index.js
@@ -1,4 +1,4 @@
-console.debug('Load WASM server module..');
+console.debug('Load WASM background module..');
 import wasm_bindgen from './pkg/background_script.js';
 wasm_bindgen("./pkg/background_script_bg.wasm")
   .then(module => {
---
+++ examples/basic-web-extension/foreground-script/src/lib.rs
@@ -0,0 +1,20 @@
+use gloo_console as console;
+use gloo_utils::{body, document};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn start() {
+    console::info!("Start foreground script");
+    let container = document().create_element("div").unwrap();
+    container.class_list().add_1("wea-example-container").unwrap();
+
+    let title = document().create_element("h2").unwrap();
+    title.set_inner_html("Example Web Extension Foreground");
+
+    let data = document().create_element("div").unwrap();
+    data.set_inner_html("Hello from foreground script");
+
+    container.append_child(&title).unwrap();
+    container.append_child(&data).unwrap();
+    body().append_child(&container).unwrap();
+}
