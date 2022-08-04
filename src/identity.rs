b9f54634be819ac2da0ce4dce910401517550cce
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-08-04 17:26:32 +0200

    fix: make private identity methods public

--- src/identity.rs
+++ src/identity.rs
@@ -15,16 +15,16 @@ extern "C" {
     pub fn get_redirect_url_with_path(this: &Identity, path: &str) -> String;
 
     #[wasm_bindgen(method, catch, js_name = launchWebAuthFlow)]
-    async fn launch_webauth_flow(this: &Identity, details: &JsValue) -> Result<JsValue, JsValue>;
+    pub async fn launch_webauth_flow(this: &Identity, details: &JsValue) -> Result<JsValue, JsValue>;
 
     #[wasm_bindgen(method, catch, js_name = getAuthToken)]
-    fn get_auth_token(this: &Identity) -> Result<(), JsValue>;
+    pub fn get_auth_token(this: &Identity) -> Result<(), JsValue>;
 
     #[wasm_bindgen(method, catch, js_name = getAuthToken)]
-    fn get_auth_token_with_details(this: &Identity, details: &JsValue) -> Result<(), JsValue>;
+    pub fn get_auth_token_with_details(this: &Identity, details: &JsValue) -> Result<(), JsValue>;
 
     #[wasm_bindgen(method, catch, js_name = getAuthToken)]
-    fn get_auth_token_with_details_and_callback(
+    pub fn get_auth_token_with_details_and_callback(
         this: &Identity,
         details: &JsValue,
         callback: &Function,
