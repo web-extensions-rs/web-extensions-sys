9118cca9ee75e9437a86ae0f029433b2c88b73a8
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-08-04 16:50:51 +0200

    Add downloads.search method

--- src/downloads.rs
+++ src/downloads.rs
@@ -2,11 +2,13 @@ use js_sys::Object;
 use wasm_bindgen::prelude::*;
 
 // TODO other methods
-#[rustfmt::skip] // rustfmt removes `async` blocks
 #[wasm_bindgen]
 extern "C" {
     pub type Downloads;
 
     #[wasm_bindgen(catch, method)]
     pub async fn download(this: &Downloads, info: &Object) -> Result<JsValue, JsValue>;
+
+    #[wasm_bindgen(catch, method)]
+    pub async fn search(this: &Downloads, query: &JsValue) -> Result<JsValue, JsValue>;
 }
