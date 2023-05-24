04dd909818f175b240313b69a179c9817f1f2ac3
Markus Kohlhase <markus.kohlhase@slowtec.de>
2023-05-24 18:34:21 +0200

    Add action.openPopup method

--- src/action.rs
+++ src/action.rs
@@ -9,4 +9,8 @@ extern "C" {
     // https://developer.chrome.com/docs/extensions/reference/action/#event-onClicked
     #[wasm_bindgen(method, getter, js_name = onClicked)]
     pub fn on_clicked(this: &Action) -> EventTarget;
+
+    // https://developer.chrome.com/docs/extensions/reference/action/#method-openPopup
+    #[wasm_bindgen(catch, method, js_name = openPopup)]
+    pub async fn open_popup(this: &Action, options: &JsValue) -> Result<JsValue, JsValue>;
 }
